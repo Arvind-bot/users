f6f025bce934571151c12cffd433784f3bc5f973
Arvind Kumar <arvindof7@gmail.com>
2020-11-03 15:56:26 +0530

    fetched user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(){
+    super();
+
+    this.state={
+      users:[]
+    };
+  }
+
+  componentDidMount(){
+    fetch("https://reqres.in/api/users?page=2")
+    .then(response=>response.json())
+    .then(users=>users.data)
+    .then(data=>this.setState({users:data}))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            hi there! 
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={()=>console.log(this.state.users)}>click</button>
+        </header>
+      </div>
+    );
+  } 
 }
 
 export default App;
